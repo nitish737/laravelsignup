f73a346520b176f5a8f300ea055ec8813c299ddb
Nitish890321 <65844730+Nitish890321@users.noreply.github.com>
2021-06-19 20:44:33 +0530

    first commit

---
+++ app/Http/Controllers/control2.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class control2 extends Controller
+{
+    //
+    function getdata(Request $req){
+        return "hello get";
+    }
+}
